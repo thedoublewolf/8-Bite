16e04f29eb9352dcc4ab8f1eb2624c87baf8d2c2
Jef Blocker <jefblocker@gmail.com>
2015-10-15 11:12:43 -0400

    Added code for Google Maps, but not updating in browser.  Will download current version from Github and add code again for testing.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 (function () {
 
-  console.log('It Works!');
+  /*let googleMapsURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop';
+
+$.ajax({
+  url: googleMapsURL,
+  dataType: 'jsonp',
+  method: 'get'
+}).then (function (response) {
+   var templateString = $('#item-template').text();
+  var renderTemplate = _.template(templateString);
+    _.each(response.results, function(item) {
+      var freshHTML = renderTemplate(item);
+      $('.location').append(freshHTML);
+//var template = $('#item-template').html();
+//var render = _.template(template);
+
+// $.getJSON('etsyURL').done( function (item) {
+//  $('.container').append( render(item) );*/
 
 }());
