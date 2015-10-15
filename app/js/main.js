95afc3201c76d386997a6aa26bcbfd39b8866e15
Hyona Kim <tekkieninja@gmail.com>
2015-10-15 14:49:35 -0400

    Added Menu items (Beer, Entrees, Games) in JS file

--- app/js/main.js
+++ app/js/main.js
@@ -1,31 +1,63 @@
-"use strict";
 
-<<<<<<< HEAD
-var pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
+// Pulling down the restaurant menu
 
-var promise = $.getJSON(pubUrl);
-promise.then(function (response) {
-  console.log(response);
-});
-=======
-// (function () {
+'use strict';
 
-// 	var newsURL = 'https://json-data.herokuapp.com/restaurant/news/1';
+(function () {
 
-// 	var promise = $.getJSON(url);
-// 	promise.then( function (response){
-// 		doSomething(response);
-// 	});
+  var pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
 
-// 	var newsInput = _.template($('#generic-form').text());
+  // Beer Items
+  var promise = $.getJSON(pubUrl);
+  promise.then(function (resp) {
+    _.each(resp.Beer, function (item) {
+      var itemHTML = loadTemplate(item);
+      $('.menu').append(itemHTML);
+    });
+  });
 
-// 	var doSomething = function (string){
+  var loadTemplate = function loadTemplate(data) {
+    var template = '\n <div class=\'beer\'>\n    <p>' + data.item + data.price + '</p>\n    <p>' + data.description + data.allergies + data.favorite + data.bottle + data.draught + '</p>\n </div>\n';
+    $('.menu').append(template);
+  };
+})();
 
-// 		_.each(string, function (news){
+// Entree Items
+(function () {
 
-// 		});
+  var pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
 
-// 	}
+  var promise = $.getJSON(pubUrl);
+  promise.then(function (resp) {
+    _.each(resp.entrees, function (item) {
+      var itemHTML = loadTemplate(item);
+      $('.menu').append(itemHTML);
+    });
+  });
 
-// }());
->>>>>>> bad243a978ab374b61df0424845a4f35c2cd829e
+  var loadTemplate = function loadTemplate(data) {
+    var template = '\n <div class=\'entrees\'>\n    <p>' + data.item + data.price + '</p>\n    <p>' + data.description + data.allergies + data.favorite + data.spicy + data.vegan + '</p>\n </div>\n';
+    console.log(template);
+    $('.menu').append(template);
+  };
+})();
+
+// Games, Games, Games
+(function () {
+
+  var pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
+
+  var promise = $.getJSON(pubUrl);
+  promise.then(function (resp) {
+    _.each(resp.games, function (item) {
+      var itemHTML = loadTemplate(item);
+      $('.menu').append(itemHTML);
+    });
+  });
+
+  var loadTemplate = function loadTemplate(data) {
+    var template = '\n <div class=\'games\'>\n    <p>' + data.item + data.price + '</p>\n    <p>' + data.description + data.favorite + data.online + data.multiplayer + data.rating + data.platform + '</p>\n </div>\n';
+    console.log(template);
+    $('.menu').append(template);
+  };
+})();
--- src/main.js
+++ src/main.js
@@ -1,10 +1,76 @@
 
-//Pulling down the restaurant menu
+// Pulling down the restaurant menu
+
+(function () {
+
+let pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
+
+// Beer Items
+let promise = $.getJSON(pubUrl);
+promise.then( function (resp) {
+  _.each(resp.Beer, function (item) {
+  var itemHTML = loadTemplate(item);
+  $('.menu').append(itemHTML);
+})
+});
+
+let loadTemplate = function (data) {
+  let template = `
+ <div class='beer'>
+    <p>${data.item}${data.price}</p>
+    <p>${data.description}${data.allergies}${data.favorite}${data.bottle}${data.draught}</p>
+ </div>
+`;
+$('.menu').append(template);
+}
+}());
+
+// Entree Items
+(function () {
+
 let pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
 
 let promise = $.getJSON(pubUrl);
-promise.then( function (response) {
-  console.log(response);
+promise.then( function (resp) {
+  _.each(resp.entrees, function (item) {
+  var itemHTML = loadTemplate(item);
+  $('.menu').append(itemHTML);
 })
+});
 
+let loadTemplate = function (data) {
+  let template = `
+ <div class='entrees'>
+    <p>${data.item}${data.price}</p>
+    <p>${data.description}${data.allergies}${data.favorite}${data.spicy}${data.vegan}</p>
+ </div>
+`;
+console.log(template)
+$('.menu').append(template);
+}
+}());
 
+// Games, Games, Games
+(function () {
+
+let pubUrl = 'https://json-data.herokuapp.com/restaurant/menu/3';
+
+let promise = $.getJSON(pubUrl);
+promise.then( function (resp) {
+  _.each(resp.games, function (item) {
+  var itemHTML = loadTemplate(item);
+  $('.menu').append(itemHTML);
+})
+});
+
+let loadTemplate = function (data) {
+  let template = `
+ <div class='games'>
+    <p>${data.item}${data.price}</p>
+    <p>${data.description}${data.favorite}${data.online}${data.multiplayer}${data.rating}${data.platform}</p>
+ </div>
+`;
+console.log(template);
+$('.menu').append(template);
+}
+}());
