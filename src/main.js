cc6b1888388ebd8a58bb0e5ae2dc0e87069cba3f
Jef Blocker <jefblocker@gmail.com>
2015-10-18 17:57:11 -0400

    Updated the JS and HTML for Flicker API to pull photos into HTML.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
-// (function () {
+(function () {
 
-// 	var promise = $.getJSON(url);
-// 	promise.then( function (response){
-// 		doSomething(response);
-// 	});
+// Flickr API Key: 56d9a62afb8167b582f1fbc74496f12c
 
-// 	var newsInput = _.template($('#generic-form').text());
+// Flickr Secret: b7726e68d7696fa8
 
-// 	var doSomething = function (string){
+// Declare Flickr URL with API Key 
+var flickrURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0b3fba2c4089dd3a68757b15128579af&tags=pub+food&format=json&nojsoncallback=1&auth_token=72157659954306922-036adcb36e5388ea&api_sig=6b4a4d13c19e6f152bf4907809d30688';
 
-// 		_.each(string, function (news){
-
-// 		});
-
-// 	}
-
-// }());
+// Get array of photos from Flicker API via json
+$.ajax({
+  url: flickrURL,
+  dataType: 'json',
+  method: 'get'
+  // Call response
+}).then (function (response) {
+  // Declare templateString variable and assign to foodPics ID
+   var templateString = $('#foodPics').text();
+  // Declare renderTemplate variable and assign to _.template() function
+  var renderTemplate = _.template(templateString);
+  // Iterate through array and pull
+    _.each(response.photos, function(pubShots) {
+      var freshHTML = renderTemplate(pubShots);
+      $('.foodPhotos').append(freshHTML);
+    }
+ });
+}());
